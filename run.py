e75e28ba503f09519f3b64df175c3990acab39ed
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-03-09 20:12:15 -0500

    Update run.py

--- run.py
+++ run.py
@@ -136,6 +136,11 @@ class ImageNetVal(object):
               self.model, loss_fn=nn.CrossEntropyLoss(reduction="sum"), eps=4.7579/1020,
               nb_iter=20, eps_iter=0.000233, rand_init=True, clip_min=-2.1179, clip_max=2.6400,
               targeted=False)
+            if FLAGS.attack=="inf4":
+              adversary = LinfPGDAttack(
+                model, loss_fn=nn.CrossEntropyLoss(reduction="sum"), eps=19.0316/255,
+                nb_iter=20, eps_iter=47.579/5100, rand_init=True, clip_min=-2.1179, clip_max=2.6400,
+                targeted=False)
             for (inp, target) in tqdm.tqdm(self.data_loader, desc=self.name):
                 target = target.to(device)
                 inp = inp.to(device)
